88ec0490a43b9231a65c45d988e292923e675fe2
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-08-17 12:52:16 +0800

    添加统计中  总信息的模块

--- templates/admin/quickpay_sale_general.dwt.php
+++ templates/admin/quickpay_sale_general.dwt.php
@@ -26,32 +26,65 @@ var templateCounts = '{$data}';
 	    <!-- {/if} -->
 	</h3>
 </div>
+<div class="row-fluid">
+	<div class="span12">
+		<ul class="list-mod list-mod-briefing move-mod-head">
+			<li class="span3">
+				<div class="bd">
+					<p class="count_p">{if $data_count.order_count}{$data_count.order_count}{else}0{/if}</p>
+					<p>订单数量（单）</p>
+				</div>
+			</li>
+			
+			<li class="span3">
+				<div class="bd">
+					<p class="count_p">¥{if $data_count.goods_amount}{$data_count.goods_amount}{else}0{/if}</p>
+					<p>消费总金额（元）</span>
+				</div>
+			</li>
+			
+			<li class="span3">
+				<div class="bd">
+					<p class="count_p">¥{if $data_count.favorable_amount}{$data_count.favorable_amount}{else}0{/if}</p>
+					<p>优惠总金额（元）</p>
+				</div>
+			</li>
+			
+			<li class="span3">
+				<div class="bd">
+					<p class="count_p">¥{if $data_count.order_amount}{$data_count.order_amount}{else}0{/if}</p>
+					<p>实付总金额（元）</p>
+				</div>
+			</li>
+		</ul>
+</div>
+</div>
 
-<form action="{$form_action}" method="post" name="searchForm">
-	<div class="row-fluid">
-		<div class="choose_list f_r">
-			<strong class="f_l">{lang key='orders::statistic.year_status_lable'}</strong>
-			{html_select_date prefix="year_begin" class="w80" time=$filter.start_time start_year="2006" end_year="+1" display_days=false display_months=false}
-			<span class="f_l">-</span>
-			{html_select_date prefix="year_end" class="w80" time=$filter.end_time start_year="2006" end_year="+1" display_days=false display_months=false}
-			<input type="submit" name="query_by_year" value="{lang key='orders::statistic.query'}" class="btn screen-btn" />
-		</div>
-	</div>
-	
-	<div class="row-fluid">
-		<div class="choose_list f_r">
-			<strong class="f_l">{lang key='orders::statistic.month_status_lable'}</strong>
-			{html_select_date prefix="month_begin" class="w80" time=$filter.start_month_time start_year="2006" end_year="+1" display_days=false field_order="YMD" month_format="%m"}
-			<span class="f_l">-</span>
-			{html_select_date prefix="month_end" class="w80" time=$filter.end_month_time start_year="2006" end_year="+1" display_days=false field_order="YMD" month_format="%m"}
-			<input type="submit" name="query_by_month" value="{lang key='orders::statistic.query'}" class="btn screen-btn1" />
-		</div>
-	</div>
-</form>
-
-<div class="row-fluid edit-page">
+<div class="row-fluid">
 	<div class="span12">
 		<div class="tabbable">
+			<form action="{$form_action}" method="post" name="searchForm">
+				<div class="row-fluid">
+					<div class="choose_list f_r">
+						<strong class="f_l">{lang key='orders::statistic.year_status_lable'}</strong>
+						{html_select_date prefix="year_begin" class="w80" time=$filter.start_time start_year="2006" end_year="+1" display_days=false display_months=false}
+						<span class="f_l">-</span>
+						{html_select_date prefix="year_end" class="w80" time=$filter.end_time start_year="2006" end_year="+1" display_days=false display_months=false}
+						<input type="submit" name="query_by_year" value="{lang key='orders::statistic.query'}" class="btn screen-btn" />
+					</div>
+				</div>
+				
+				<div class="row-fluid">
+					<div class="choose_list f_r">
+						<strong class="f_l">{lang key='orders::statistic.month_status_lable'}</strong>
+						{html_select_date prefix="month_begin" class="w80" time=$filter.start_month_time start_year="2006" end_year="+1" display_days=false field_order="YMD" month_format="%m"}
+						<span class="f_l">-</span>
+						{html_select_date prefix="month_end" class="w80" time=$filter.end_month_time start_year="2006" end_year="+1" display_days=false field_order="YMD" month_format="%m"}
+						<input type="submit" name="query_by_month" value="{lang key='orders::statistic.query'}" class="btn screen-btn1" />
+					</div>
+				</div>
+			</form>
+		
 			<ul class="nav nav-tabs">
 				<li class="{if $page eq 'init'}active{/if}"><a class="data-pjax" href='{url path="quickpay/admin_sale_general/init"}'>{lang key='orders::statistic.order_status'}</a></li>
 				<li class="{if $page eq 'sales_trends'}active{/if}"><a class="data-pjax" href='{url path="quickpay/admin_sale_general/sales_trends"}'>{lang key='orders::statistic.turnover_status'}</a></li>
