45e8caff7a5e5c66101d0726009bcfa71ab1aeec
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-09-26 10:31:40 +0800

    优化格式

--- templates/admin/quickpay_config.dwt.php
+++ templates/admin/quickpay_config.dwt.php
@@ -8,30 +8,28 @@
 <!-- {/block} -->
 
 <!-- {block name="admin_config_form"} -->
-<div class="row-fluid edit-page">
-	<div class="row-fluid">
-		<form method="post" class="form-horizontal" action="{$form_action}" name="theForm" >
-			<fieldset>
-				<div>
-					<h3 class="heading">
-						<!-- {if $ur_here}{$ur_here}{/if} -->
-					</h3>
+<div class="row-fluid">
+	<form method="post" class="form-horizontal" action="{$form_action}" name="theForm" >
+		<fieldset>
+			<div>
+				<h3 class="heading">
+					<!-- {if $ur_here}{$ur_here}{/if} -->
+				</h3>
+			</div>
+			
+			<div class="control-group formSep">
+				<label class="control-label">规则描述：</label>
+				<div class="controls">
+					<textarea class="span7" name="quickpay_rule" >{$quickpay_rule}</textarea>
+					<span class="input-must"><span class="require-field">*</span></span>
 				</div>
-				
-				<div class="control-group formSep">
-					<label class="control-label">规则描述：</label>
-					<div class="controls">
-						<textarea class="span7" name="quickpay_rule" >{$quickpay_rule}</textarea>
-						<span class="input-must"><span class="require-field">*</span></span>
-					</div>
+			</div>
+			<div class="control-group">
+				<div class="controls">
+					<input type="submit" value="确定" class="btn btn-gebo" />
 				</div>
-				<div class="control-group">
-					<div class="controls">
-						<input type="submit" value="确定" class="btn btn-gebo" />
-					</div>
-				</div>
-			</fieldset>
-		</form>
-	</div>
+			</div>
+		</fieldset>
+	</form>
 </div>
 <!-- {/block} -->
