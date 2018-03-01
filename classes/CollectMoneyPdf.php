c31a4f1cf948677a18f19b3b1b24965105b24b8d
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-03-01 10:31:13 +0800

    更新收款码生成PDF类

--- classes/CollectMoneyPdf.php
+++ classes/CollectMoneyPdf.php
@@ -4,10 +4,22 @@ namespace Ecjia\App\Quickpay;
 
 class CollectMoneyPdf
 {
-    
-    public function xx()
+    /**
+     * 生成收款码PDF
+     *
+     * @param string $merchants_name
+     * @param string $store_logo    url
+     * @param string $collectmoney_qrcode   url
+     * @return string
+     */
+    public function make($merchants_name, $store_logo, $collectmoney_qrcode)
     {
-        
+        $html = $this->formatHtml($merchants_name, $store_logo, $collectmoney_qrcode);
+        $this->createPDF($html);
+    }
+    
+    public function createPDF($html)
+    {
         $pdf = royalcms('tcpdf');
         // 设置文档信息
         $pdf->SetCreator('收款二维码');
@@ -36,30 +48,35 @@ class CollectMoneyPdf
         
         //第一页
         $pdf->AddPage();
-        
-//         $pdf->writeHTML('<div style="text-align: center"><h1>第一页内容</h1></div>');
-//         $pdf->writeHTML('<p>我是第一行内容</p>');
-//         $pdf->writeHTML('<p style="color: red">我是第二行内容</p>');
-//         $pdf->writeHTML('<p>我是第三行内容</p>');
-//         $pdf->Ln(5);//换行符
-//         $pdf->writeHTML('<p><img width="500" height="500" src="https://cityo2o.ecjia.com/content/uploads/merchant/60/data/shop_logo/1477948615542668810.png" /></p>');
-        
-        $pdf->writeHTML('<div class="left-side" style="width: 300px;height: 430px;border: 1px solid #eee;float: left;background-color: #379ED8;border-radius: 8px;text-align: center;">');
-        $pdf->writeHTML('<div class="store-logo"><img src="'+ $store_logo +'" style="width: 60px;height: 60px;border-radius: 50%;margin: 20px 0 10px 0;"></div>');
-        $pdf->writeHTML('<div class="store-name" style="font-size: 18px;color: #fff;margin: 10px 0;">'+ $merchants_name +'</div>');
-        $pdf->writeHTML('<div class="qrcode" style="margin-top: 20px;"><img src="'+ $collectmoney_qrcode +'" style="width: 220px;height: 220px;"></div>');
-        $pdf->writeHTML('<div class="info" style="color: #fff;font-size: 15px;margin-top: 20px;">微信扫描二维码进行买单</div>');
-        $pdf->writeHTML('</div>');
+        $pdf->writeHTML($html);
         
         //输出PDF
-        $pdf->Output('t.pdf', 'I');
-        
-
-
+        $pdf->Output('ecjia_collect_money.pdf', 'I');
     }
     
-    
-    
-    
-    
+    /**
+     * 生成收款码HTML页面
+     * 
+     * @param string $merchants_name
+     * @param string $store_logo    url
+     * @param string $collectmoney_qrcode   url
+     * @return string
+     */
+    public function formatHtml($merchants_name, $store_logo, $collectmoney_qrcode)
+    {
+        $html = <<<EOL
+        <div class="left-side" style="width: 300px;height: 430px;border: 1px solid #eee;float: left;background-color: #379ED8;border-radius: 8px;text-align: center;">
+            <div class="store-logo">
+                <img src="'+ $store_logo +'" style="width: 60px;height: 60px;border-radius: 50%;margin: 20px 0 10px 0;">
+            </div>
+            <div class="store-name" style="font-size: 18px;color: #fff;margin: 10px 0;">'+ $merchants_name +'</div>
+            <div class="qrcode" style="margin-top: 20px;">
+                <img src="'+ $collectmoney_qrcode +'" style="width: 220px;height: 220px;">
+            </div>
+            <div class="info" style="color: #fff;font-size: 15px;margin-top: 20px;">微信扫描二维码进行买单</div>
+        </div>
+EOL;
+        return $html;
+    }
+ 
 }
