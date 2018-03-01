65ca37c9e305afd325c5b025129ce4c0ab691eac
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-03-01 16:58:05 +0800

    修改二维码店铺logo

--- mh_qrcode.php
+++ mh_qrcode.php
@@ -86,6 +86,8 @@ class mh_qrcode extends ecjia_merchant {
         $this->assign('print_url', RC_Uri::url('quickpay/mh_qrcode/print_qrcode'));
         
         $merchant_info['collectmoney_qrcode'] = with(new Ecjia\App\Mobile\Qrcode\GenerateCollectMoney($_SESSION['store_id'],  $merchant_info['shop_logo']))->getQrcodeUrl();
+        $merchant_info['shop_logo'] = RC_Upload::upload_url($merchant_info['shop_logo']);
+        
         $this->assign('merchant_info', $merchant_info);
         
 		$this->display('quickpay_qrcode.dwt');
