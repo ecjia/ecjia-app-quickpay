6304f88c4a1ae15da525a981328232d1cdb99446
东方皓 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-03-01 16:44:28 +0800

    更新收款码PDF生成样式

--- classes/CollectMoneyPdf.php
+++ classes/CollectMoneyPdf.php
@@ -4,21 +4,45 @@ namespace Ecjia\App\Quickpay;
 
 class CollectMoneyPdf
 {
+    protected $store_name;
+    
+    protected $store_logo;
+    
+    protected $collectmoney_qrcode;
+    
     /**
-     * 生成收款码PDF
-     *
-     * @param string $merchants_name
+     * 
+     * @param string $store_name
      * @param string $store_logo    url
      * @param string $collectmoney_qrcode   url
-     * @return string
      */
-    public function make($merchants_name, $store_logo, $collectmoney_qrcode)
+    public function __construct($store_name, $store_logo, $collectmoney_qrcode)
     {
-        $html = $this->formatTableHtml($merchants_name, $store_logo, $collectmoney_qrcode);
-        $this->createPDF($html);
+        $this->store_name = $store_name;
+        $this->store_logo = $store_logo;
+        $this->collectmoney_qrcode = $collectmoney_qrcode;
     }
     
-    public function createPDF($html)
+    
+    /**
+     * 生成收款码PDF
+     * 
+     * @param string $output   
+     * Dest：PDF输出的方式。
+     *      I，默认值，在浏览器中打开；
+     *      D，点击下载按钮， PDF文件会被下载下来；
+     *      F，文件会被保存在服务器中；
+     *      S，PDF会以字符串形式输出；
+     *      E：PDF以邮件的附件输出。
+     * @return string
+     */
+    public function make($output)
+    {
+        $html = $this->formatTableHtml();
+        $this->createPDF($html, $output);
+    }
+    
+    public function createPDF($html, $output = 'I')
     {
         $pdf = royalcms('tcpdf');
         // 设置文档信息
@@ -50,21 +74,21 @@ class CollectMoneyPdf
         $pdf->AddPage();
         $pdf->writeHTML($html, true, false, true, false, '');
         
-        //输出PDF
-        $pdf->Output('ecjia_collect_money.pdf', 'I');
+        /* 输入PDF文档 :
+        Name：PDF保存的名字
+        Dest：PDF输出的方式。I，默认值，在浏览器中打开；D，点击下载按钮， PDF文件会被下载下来；F，文件会被保存在服务器中；S，PDF会以字符串形式输出；E：PDF以邮件的附件输出。 
+        */
+        $pdf->Output('ecjia_collect_money.pdf', $output);
     }
     
     /**
      * 生成收款码HTML页面
      *
-     * @param string $merchants_name
-     * @param string $store_logo    url
-     * @param string $collectmoney_qrcode   url
      * @return string
      */
-    public function formatTableHtml($merchants_name, $store_logo, $collectmoney_qrcode)
+    public function formatTableHtml()
     {
-        $collectHtml = $this->formatHtml($merchants_name, $store_logo, $collectmoney_qrcode);
+        $collectHtml = $this->formatHtml();
         
         $tablehtml = <<<EOL
         <table>
@@ -92,18 +116,15 @@ EOL;
     /**
      * 生成收款码HTML页面
      * 
-     * @param string $merchants_name
-     * @param string $store_logo    url
-     * @param string $collectmoney_qrcode   url
      * @return string
      */
-    public function formatHtml($merchants_name, $store_logo, $collectmoney_qrcode)
+    public function formatHtml()
     {
         $html = <<<EOL
         <div style="border: 2px solid #eee; float: left; background-color: #379ED8; text-align: center;">
-            <div><img src="{$store_logo}" style="width: 200px;height: 200px;"></div>
-            <div style="font-size: 18pt;color: #fff; width: 500px;">{$merchants_name}</div>
-            <div style="margin-top: 20px;"><img src="{$collectmoney_qrcode}" style="width: 530px;height: 530px;"></div>
+            <div><img src="{$this->store_logo}" style="width: 200px;height: 200px;"></div>
+            <div style="font-size: 18pt;color: #fff; width: 500px;">{$this->store_name}</div>
+            <div style="margin-top: 20px;"><img src="{$this->collectmoney_qrcode}" style="width: 530px;height: 530px;"></div>
             <div style="color: #fff;font-size: 12pt; margin-top: 20px;">微信扫描二维码进行买单</div>
         </div>
 EOL;
