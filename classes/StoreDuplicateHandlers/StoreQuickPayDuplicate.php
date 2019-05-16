1cdddb2883f9b3cba35a84efb35ef44a5b4993be
李文浩 <huangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-05-16 15:35:35 +0800

    调整店铺复制列表显示顺序，按依赖规则由上至下排序。 PS：各模块分配的排序值为： store[1,10]; goods[11,30]; bonus[31,40]; quickpay[41,50]; shipping[51,60]

--- classes/StoreDuplicateHandlers/StoreQuickPayDuplicate.php
+++ classes/StoreDuplicateHandlers/StoreQuickPayDuplicate.php
@@ -33,7 +33,7 @@ class StoreQuickPayDuplicate extends StoreDuplicateAbstract
      * 排序
      * @var int
      */
-    protected $sort = 8;
+    protected $sort = 41;
 
     public function __construct($store_id, $source_store_id)
     {
@@ -74,14 +74,41 @@ HTML;
      */
     public function handleDuplicate()
     {
-        $item = $this->dependentCheck();
-        //判断提示错误
-        if (empty($item)){
-            //标记处理完成
-            $this->markDuplicateFinished();
-            //$this->handleAdminLog();
-            return TRUE;
+        //检测当前对象是否已复制完成
+        if ($this->isCheckFinished()){
+            return true;
         }
+
+        $dependent = false;
+        if (!empty($this->dependents)) { //如果设有依赖对象
+            //检测依赖
+            if (!empty($this->dependentCheck())){
+                $dependent = true;
+            }
+        }
+
+        //如果当前对象复制前仍存在依赖，则需要先复制依赖对象才能继续复制
+        if ($dependent){
+            return false;
+        }
+
+        //@todo 执行具体任务
+        $this->startDuplicateProcedure();
+
+        //标记处理完成
+        $this->markDuplicateFinished();
+
+        //记录日志
+        $this->handleAdminLog();
+
+        return true;
+    }
+
+    /**
+     * 此方法实现店铺复制操作的具体过程
+     */
+    protected function startDuplicateProcedure(){
+
     }
 
     /**
