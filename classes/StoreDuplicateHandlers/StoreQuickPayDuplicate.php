de18ce98dfa6f1d8e7431bb1a4fd37bfe3454887
李文浩 <huangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-05-24 15:40:54 +0800

    更新店铺复制操作日志内容

--- classes/StoreDuplicateHandlers/StoreQuickPayDuplicate.php
+++ classes/StoreDuplicateHandlers/StoreQuickPayDuplicate.php
@@ -179,7 +179,7 @@ HTML;
         }
 
         \Ecjia\App\Store\Helper::assign_adminlog_content();
-        $content = sprintf(__('录入：将【%s】店铺所有优惠买单规则复制到【%s】店铺中', 'goods'), $source_store_merchant_name, $store_merchant_name);
+        $content = sprintf(__('将【%s】店铺所有优惠买单规则复制到【%s】店铺中', 'goods'), $source_store_merchant_name, $store_merchant_name);
         ecjia_admin::admin_log($content, 'duplicate', 'store_goods');
     }
 }
